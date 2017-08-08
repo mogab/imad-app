9a351afe81273a994c060262020d3509657f140e
mogab <30411007+mogab@users.noreply.github.com>
2017-08-07 22:50:33 -0700

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -8,5 +8,5 @@ element.onclick = function(){
 var img = document.getElementById("madi");
 img.onclick = function(){
     console.log("Image Clicked!");
-    img.style.marginleft = '150px';
+    img.style.marginLeft = '150px';
 };
