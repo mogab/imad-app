e9141a87b0d1aa6032d479e86963001b4d9957a2
mogab <30411007+mogab@users.noreply.github.com>
2017-08-07 23:03:42 -0700

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -5,7 +5,13 @@ element.onclick = function(){
     element.innerHTML = 'Hello World!';
 };
 //Move image
+
+var marginLeft = 10;
+function moveRight(){
+    marginLeft = marginLeft+1;
+    img.style.marginLeft = marginLef + "px";
+}
 var img = document.getElementById("madi");
 img.onclick = function(){
-    img.style.marginLeft = '150px';
+    var interval = setInterval(moveRight, 50);
 };
