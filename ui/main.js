0250478c7810a73b6abec57c13d9fabab2cef643
mogab <30411007+mogab@users.noreply.github.com>
2017-08-07 22:18:20 -0700

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,3 +1,9 @@
 console.log('Loaded!');
 var element = document.getElementById("main-text");
 element.innerHTML = "Mera Duniya!";
+
+//Move image
+var img = document.getElementById("madi");
+img.onclcik = function(){
+    img.style.marginleft="100px";
+};
